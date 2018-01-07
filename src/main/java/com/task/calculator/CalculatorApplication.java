107000428648be5c0432efe3b54549d027e4e930
Wojciech Figas <wojciech.figas@outlook.com>
2018-01-07 17:13:17 +0100

    Adding provided data on startup

--- src/main/java/com/task/calculator/CalculatorApplication.java
+++ src/main/java/com/task/calculator/CalculatorApplication.java
@@ -1,12 +1,58 @@
 package com.task.calculator;
 
+import com.task.calculator.dao.CountryCostsInformationRepository;
+import com.task.calculator.entity.CountryCostsInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class CalculatorApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
+    private static Logger LOGGER = LoggerFactory.getLogger(CalculatorApplication.class);
+
+    @Autowired
+    private CountryCostsInformationRepository countryCostsInformationRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        CountryCostsInformation UK = new CountryCostsInformation();
+        UK.setCountryCode("UK");
+        UK.setCurrencyCode("GBP");
+        UK.setFixedCosts(600);
+        UK.setIncomeTaxPercentage(0.25);
+
+        CountryCostsInformation DE = new CountryCostsInformation();
+        DE.setCountryCode("DE");
+        DE.setCurrencyCode("EUR");
+        DE.setFixedCosts(800);
+        DE.setIncomeTaxPercentage(0.20);
+
+        CountryCostsInformation PL = new CountryCostsInformation();
+        PL.setCountryCode("PL");
+        PL.setCurrencyCode("PLN");
+        PL.setFixedCosts(1200);
+        PL.setIncomeTaxPercentage(0.19);
+
+        LOGGER.info("Adding provided data");
+        countryCostsInformationRepository.save(Arrays.asList(UK, DE, PL));
+
+    }
 }
